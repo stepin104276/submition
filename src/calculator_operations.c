38691b2d18084e045ada46f06126c76f6579ebae
99002526 <71489099+99002526@users.noreply.github.com>
2020-09-19 00:02:23 +0530

    Update calculator_operations.c

--- src/calculator_operations.c
+++ src/calculator_operations.c
@@ -1,34 +1,48 @@
 #include <calculator_operations.h>
-int sum;
 
 int add(int operand1, int operand2)
 {
+    int sum;
     sum = operand1 + operand2;
     return sum;
 }
 
 int subtract(int operand1, int operand2)
 {
-    return operand1 - operand2;
+    int sum;
+    sum = operand1 - operand2
+    return sum;
 }
 
 int multiply(int operand1, int operand2)
 {
-    return operand1 * operand2;
+    int result;
+    result = operand1 * operand2
+    return result;
 }
 
 int divide(int operand1, int operand2)
 {
+    int result;
     if(0 == operand2)
+    {
         return 0;
+    }
     else
-        return operand1 / operand2;
+    {
+        result = operand1 / operand2
+        return result;
+    }
 }
 
 int greater(int operand1, int operand2)
 {
     if(operand1>operand2)
+    {
         return operand1;
+    }
     else
+    {
         return operand2;
+    }
 }
