dc8e3542717a33d433ce95e0d71cb679df0a4ce1
99002526 <71489099+99002526@users.noreply.github.com>
2020-09-19 00:08:38 +0530

    Update calculator_operations.c

--- src/calculator_operations.c
+++ src/calculator_operations.c
@@ -46,3 +46,15 @@ int greater(int operand1, int operand2)
         return operand2;
     }
 }
+
+int lesser(int operand1, int operand2)
+{
+    if(operand1<operand2)
+    {
+        return operand1;
+    }
+    else
+    {
+        return operand2;
+    }
+}
